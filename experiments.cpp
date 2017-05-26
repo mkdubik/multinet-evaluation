8cae07856e50e0eb28d09eae4240f3f4f82e5872
Mikael Dubik <mkdubik@gmail.com>
2017-05-26 14:35:06 +0200

    Testing should be complete!

--- experiments.cpp
+++ experiments.cpp
@@ -14,30 +14,34 @@ struct stats
 {
 	double modul = 0;
 	double nmi = 0;
+	double community_ratio = 0.0;
 	std::string path;
 
 	void write() {
 		std::ofstream ofs;
 		ofs.open(result_path + path, std::ofstream::out | std::ofstream::trunc);
-		ofs << modul << "," << nmi << std::endl;
+		ofs << modul << "," << nmi << "," << community_ratio << std::endl;
 		ofs.close();
 	}
 
 };
 
-void write(mlnet::CommunityStructureSharedPtr c, std::string path) {
-	if ((*c).get_communities().size() == 0) {
-		return;
-	}
-
-	std::ofstream ofs;
-	ofs.open(result_path + path + "_community", std::ofstream::out | std::ofstream::trunc);
-	(*c).print(ofs);
-	ofs.close();
-}
-
 double modul(mlnet::MLNetworkSharedPtr mnet, mlnet::CommunityStructureSharedPtr c) {
 	return 0;
+}
+
+double comm(mlnet::CommunityStructureSharedPtr c, mlnet::CommunityStructureSharedPtr truth) {
+	if ((*c).get_communities().size() == 0) {
+		return -1;
+	}
+	if ((*truth).get_communities().size() == 0) {
+		return -1;
+	}
+
+	double c_sz = static_cast<int>((*c).get_communities().size());
+	double truth_sz = static_cast<int>((*c).get_communities().size());
+
+	return c_sz / truth_sz;
 }
 
 double nmi(mlnet::CommunityStructureSharedPtr c, mlnet::CommunityStructureSharedPtr truth, int n) {
@@ -48,6 +52,51 @@ double nmi(mlnet::CommunityStructureSharedPtr c, mlnet::CommunityStructureShared
 		return -1;
 	}
 	return normalized_mutual_information(c, truth, n);
+}
+
+
+mlnet::CommunityStructureSharedPtr read_truth_overlap(mlnet::MLNetworkSharedPtr mnet) {
+	std::string truth_path = "truth/";
+
+	std::fstream file((truth_path + mnet->name), std::ios_base::in);
+	std::map<unsigned int, std::vector<unsigned int>> mapped;
+
+	std::string str;
+    while(getline(file, str))
+    {
+        std::istringstream ss(str);
+        unsigned int num;
+
+    	int actor_id = 0;
+		/*
+			map from:
+				aid cid cid cid
+			to:
+				cid aid aid aid
+
+		*/
+        while(ss >> num) {
+        	if (actor_id == 0){
+	        	actor_id = num;
+        	} else {
+        		mapped[num].push_back(actor_id);
+        	}
+        }
+    }
+
+	mlnet::CommunityStructureSharedPtr communities = mlnet::community_structure::create();
+	for(const auto &p : mapped) {
+		mlnet::CommunitySharedPtr c = mlnet::community::create();
+
+		for (auto m: p.second) {
+			for (mlnet::NodeSharedPtr n : *mnet->get_nodes(((mnet->get_actors()->get_at_index(m - 1))))) {
+				(*c).add_node(n);
+			}
+		}
+		(*communities).add_community(c);
+	}
+
+	return communities;
 }
 
 mlnet::CommunityStructureSharedPtr read_truth(mlnet::MLNetworkSharedPtr mnet) {
@@ -73,7 +122,8 @@ mlnet::CommunityStructureSharedPtr read_truth(mlnet::MLNetworkSharedPtr mnet) {
 void tLART(mlnet::MLNetworkSharedPtr mnet, mlnet::CommunityStructureSharedPtr truth) {
 	mlnet::lart l;
 	std::string format = mnet->name + "/LART_";
-	std::vector<int> t = {2, 3, 6, 9};
+
+	std::vector<int> t = {2, 3, 6, 9, 12};
 	std::vector<double> eps = {0.1, 0.5, 1.0};
 	std::vector<double> gamma = {0.25, 0.5, 1.0, 1.5, 2.0, 2.5};
 
@@ -88,10 +138,12 @@ void tLART(mlnet::MLNetworkSharedPtr mnet, mlnet::CommunityStructureSharedPtr tr
 							std::to_string(gamma[k]);
 				mlnet::CommunityStructureSharedPtr c = l.fit(mnet, t[i], eps[j], gamma[k]);
 
+				std::cout << "Finished !" << std::endl;
+
 				s.modul = modul(mnet, c);
 				s.nmi = nmi(c, truth, mnet->get_nodes()->size());
+				s.community_ratio = comm(c, truth);
 				s.write();
-				write(c, s.path);
 			}
 		}
 	}
@@ -114,8 +166,9 @@ void tGLOUVAIN(mlnet::MLNetworkSharedPtr mnet, mlnet::CommunityStructureSharedPt
 							method[i];
 				mlnet::CommunityStructureSharedPtr c = g.fit(mnet, method[i], gamma[j], omega[k]);
 				s.modul = modul(mnet, c);
+				s.nmi = nmi(c, truth, mnet->get_nodes()->size());
+				s.community_ratio = comm(c, truth);
 				s.write();
-				write(c, s.path);
 			}
 		}
 	}
@@ -128,24 +181,15 @@ void tPMM(mlnet::MLNetworkSharedPtr mnet, mlnet::CommunityStructureSharedPtr tru
 	std::vector<int> k = {5, 10, 20, 40, 60};
 
 	for (size_t i = 0; i < k.size(); i++) {
-		std::vector<int> ell = {1, k[i] / 2, k[i], k[i] + 2, k[i] * 2};
-
-		for (size_t j = 0; i < ell.size(); j++) {
-			if (j > 4 && mnet->name == "aucs") {
-				break;
-			}
-
-			stats s;
-			s.path = format +
-						std::to_string(k[i]) + "_" +
-						std::to_string(ell[j]);
-			mlnet::CommunityStructureSharedPtr c1 = p.fit(mnet, k[i], 1);
-			s.modul = modul(mnet, c1);
-			s.nmi = nmi(c1, truth, mnet->get_nodes()->size());
-			s.write();
-
-			write(c1, s.path);
-		}
+		stats s;
+		s.path = format +
+					std::to_string(k[i]) + "_" +
+					std::to_string(k[i] * 2);
+		mlnet::CommunityStructureSharedPtr c = p.fit(mnet, k[i], k[i] * 2);
+		s.modul = modul(mnet, c);
+		s.nmi = nmi(c, truth, mnet->get_nodes()->size());
+		s.community_ratio = comm(c, truth);
+		s.write();
 	}
 }
 
@@ -158,28 +202,35 @@ int main() {
 	}
 
 	std::vector<std::tuple<std::string, std::string, bool>> datasets = {
-		std::make_tuple("aucs", ",", true),
-		std::make_tuple("dk", ",", true),
-		std::make_tuple("1k_mix025", " ", false),
 		std::make_tuple("1k_mix04", " ", true),
-		std::make_tuple("3k_mix025", " ", true),
 		std::make_tuple("3k_mix04", " ", true),
 		std::make_tuple("5k_mix025", " ", true),
+		std::make_tuple("3k_overlap_2250_5", " ", true),
+		std::make_tuple("5k_overlap_3750_5", " ", true),
+		std::make_tuple("aucs", ",", true),
+		std::make_tuple("dk", ",", true),
 		//std::make_tuple("fftwyt", " ", false),
 		//std::make_tuple("eu", " ", false),
 	};
+	std::string ix = "overlap";
 
 	for (size_t i = 0; i < datasets.size(); i++) {
 		if(system(("mkdir -p " + result_path + std::get<0>(datasets[i])).c_str())) {
 			return 1;
 		}
 
-		mlnet::MLNetworkSharedPtr mnet = mlnet::read_multilayer(data_path + std::get<0>(datasets[i]),
+		std::string dataset = std::get<0>(datasets[i]);
+		mlnet::MLNetworkSharedPtr mnet = mlnet::read_multilayer(data_path + dataset,
 			std::get<0>(datasets[i]), std::get<1>(datasets[i])[0]);
 
 		mlnet::CommunityStructureSharedPtr truth;
 		if (std::get<2>(datasets[i])) {
-			truth = read_truth(mnet);
+
+			if (dataset.find(ix) != std::string::npos) {
+				truth = read_truth_overlap(mnet);
+			} else {
+				truth = read_truth(mnet);
+			}
 		} else {
 			truth = mlnet::community_structure::create();
 		}
---
+++ telemetry/aggregate.py
@@ -0,0 +1,126 @@
+import sys
+from pdb import set_trace
+
+
+class aggregate_results:
+
+	def aggregate():
+		data = {}
+		with open(sys.argv[1]) as fd:
+			data = eval(fd.read().replace(',\n]', ']').replace('},\n]', '}]\n'))
+
+		methods = {
+			"lart": self.process_lart,
+			"glouvain": self.process_glouvain,
+			"pmm": self.process_pmm
+		}
+
+		for obj in data:
+			func = methods[obj['method']]
+			func(obj)
+
+		return obj
+
+
+
+	def process_memory_log(self, contents):
+		mem = [item for item in contents.split('\n')]
+		for m in mem:
+			k, v = m.split(':')
+			if k == 'VmRSS':
+				return v.replace('\t', '').split()[0]
+
+		raise Exception(contents + " missing VmRSS")
+
+	def process_accuracy(self, filename):
+		accuracy = {}
+		with open(filename) as fd:
+			modularity, nmi, comm_ratio = fd.read().replace('\n', '').split(',')
+			accuracy['modularity'] = modularity
+			accuracy['nmi'] = nmi
+			accuracy['comm_ratio'] = comm_ratio
+
+		return accuracy
+
+
+	def process_lart(self, obj):
+		fl = '{function}_{dataset}_{walks}_{eps}_{gamma}.log'
+		memory = {}
+
+		for func in obj['timestamps'].keys():
+			if func == 'total':
+				func = 'end'
+
+			tmp = fl.format(function = func,
+							dataset = obj['params']['dataset'],
+							walks = obj['params']['walks'],
+							eps = obj['params']['eps'],
+							gamma = obj['params']['gamma'])
+
+			with open('telemetry/lart/' + tmp) as fd:
+				memory[func] = self.process_memory_log(fd.read())
+
+		obj['memory'] = memory
+		fl = 'results/{dataset}/LART_{walks}_{eps}_{gamma}'.format(
+							dataset = obj['params']['dataset'],
+							walks = obj['params']['walks'],
+							eps = obj['params']['eps'],
+							gamma = obj['params']['gamma'])
+
+		obj['accuracy'] = self.process_accuracy(fl)
+
+
+	def process_glouvain(self, obj):
+		fl = 'pass_{p}_{dataset}_{move}_{gamma}_{omega}.log'
+		memory = {}
+
+		for p in obj['passes'].keys():
+			tmp = fl.format(p = p,
+							dataset = obj['params']['dataset'],
+							move = obj['params']['move'],
+							gamma = obj['params']['gamma'],
+							omega = obj['params']['omega'])
+
+			with open('telemetry/glouvain/' + tmp) as fd:
+				memory[p] = process_memory_log(fd.read())
+
+		obj['memory'] = memory
+
+		fl = 'results/{dataset}/GLOUVAIN_{gamma}_{omega}_{move}'.format(
+							dataset = obj['params']['dataset'],
+							gamma = obj['params']['gamma'],
+							omega = obj['params']['omega'],
+							move = obj['params']['move'])
+
+		obj['accuracy'] = self.process_accuracy(fl)
+
+
+	def process_pmm(self, obj):
+		svd = 'svd_{dataset}_{k}_{ell}.log'
+		kmeans = 'kmeans_{dataset}_{k}_{ell}.log'
+		memory = {}
+
+		if 'error' in obj['params']:
+			return
+
+		tmp = svd.format(dataset = obj['params']['dataset'],
+						k = obj['params']['k'],
+						ell = obj['params']['ell'])
+
+		with open('telemetry/pmm/' + tmp) as fd:
+			memory['svd'] = self.process_memory_log(fd.read())
+
+		tmp = kmeans.format(dataset = obj['params']['dataset'],
+						k = obj['params']['k'],
+						ell = obj['params']['ell'])
+
+		with open('telemetry/pmm/' + tmp) as fd:
+			memory['kmeans'] = process_memory_log(fd.read())
+
+		fl = 'results/{dataset}/PMM_{k}_{ell}'.format(
+							dataset = obj['params']['dataset'],
+							gamma = obj['params']['k'],
+							omega = obj['params']['ell'])
+
+		obj['accuracy'] = self.process_accuracy(fl)
+
